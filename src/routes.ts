81bb9813e5c336a5134c828d89e30b814290fbf5
augustorodrigues <jrodriguesaugusto@gmail.com>
2019-09-02 04:36:37 -0300

    removing comentaries

--- src/routes.ts
+++ src/routes.ts
@@ -10,16 +10,12 @@ import ValidateDates from './app/middlewares/ValidadeDates'
 const routes = Router();
 const database = 'C:/cubos/desafio-backend/src/database/database.json';
 
-// findAll
 routes.get('/rules', ScheduleRuleController.findAll);
 
-// filterDates
 routes.post('/rules/availableHours', ValidateDates, ScheduleRuleController.findFilterDates);
 
-// save
 routes.post('/rules', ValidateScheduleRule, ValidateInterval, ScheduleRuleController.save);
 
-// delete
 routes.delete('/rules/:id', ValidadeScheduleExists, ScheduleRuleController.delete);
 
 export default routes;
